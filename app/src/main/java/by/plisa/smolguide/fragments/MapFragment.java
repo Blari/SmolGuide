ed3e57bd9531098c34b1b0f17c10c4a5448fe1fc
lehakorshun <lehakorshun@gmail.com>
2015-08-07 01:07:56 +0600

    change map fragment

--- app/src/main/java/by/plisa/smolguide/fragments/MapFragment.java
+++ app/src/main/java/by/plisa/smolguide/fragments/MapFragment.java
@@ -2,30 +2,27 @@ package by.plisa.smolguide.fragments;
 
 
 import android.os.Bundle;
-        import android.support.annotation.Nullable;
-        import android.view.View;
-import android.view.ViewTreeObserver;
+import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
-        import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-        import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-        import com.google.android.gms.maps.model.CameraPosition;
-        import com.google.android.gms.maps.model.LatLng;
-        import com.google.android.gms.maps.model.Marker;
-        import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-        import by.plisa.smolguide.R;
-        import by.plisa.smolguide.models.Pin;
-        import by.plisa.smolguide.utils.PinsApiInterface;
+import by.plisa.smolguide.R;
+import by.plisa.smolguide.models.Pin;
+import by.plisa.smolguide.utils.PinsApiInterface;
 
-        import java.util.List;
+import java.util.List;
 
-        import retrofit.Callback;
-        import retrofit.RestAdapter;
-        import retrofit.RetrofitError;
-        import retrofit.client.Response;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
